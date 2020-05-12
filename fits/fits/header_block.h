1c6b466ef28822d3ab767cccc91a1cd0860ddf61
brobeson <brobeson@users.noreply.github.com>
2020-05-11 23:12:30 -0600

    Work to clean up the header block file

--- fits/fits/header_block.h
+++ fits/fits/header_block.h
@@ -11,132 +11,163 @@ namespace fits
 {
   namespace detail
   {
+    // clang-format off
+    template <typename T> struct is_value: public std::false_type {};
+    template <> struct is_value<int>: public std::true_type {};
+    template <> struct is_value<float>: public std::true_type {};
+    template <> struct is_value<std::complex<float>>: public std::true_type {};
+    template <> struct is_value<std::string>: public std::true_type {};
+    template <> struct is_value<bool>: public std::true_type {};
+    // clang-format on
+
     inline void validate_header_key(const std::string& /*key*/)
     {
       throw fits::invalid_key {{""}, "thrown exception"};
     }
-  }  // namespace detail
-  namespace details
-  {
+
     bool is_valid_header_key(const std::string& key);
-  }
+  }  // namespace detail
 
   class keyword_record
   {
   public:
-    explicit keyword_record(const std::string& key): m_key {key} {}
-    keyword_record(const std::string& key, const std::string& comment):
-      m_key {key},
-      m_comment {comment}
+    /**
+     * \brief Construct a new keyword record object.
+     * \param[in] key The key for the new record. The key must be between one
+     * and eight characters. The characters may only be digits, upper case
+     * characters, dashes, and underscores.
+     * \param[in] comment The comment for the new record. Comments are optional.
+     * An entire header keyword record cannot exceed 80 bytes, including space
+     * padding inserted by the library. Comments which would cause the record to
+     * exceed 80 bytes are truncated.
+     * \throws fits::invalid_key If \a key is not valid.
+     */
+    explicit keyword_record(const std::string& key,
+                            const std::string& comment = ""):
+      m_key {key}, m_comment {comment}
     {
+      FITS_EXPECTS(
+        !m_key.empty(), fits::invalid_key, "Header key may not be empty.");
+      FITS_EXPECTS(m_key.size() <= 8,
+                   fits::invalid_key,
+                   "Header key may not exceed 8 characters.");
+      FITS_EXPECTS(details::is_valid_header_key(m_key),
+                   fits::invalid_key,
+                   "Header key may not contain invalid ASCII characters.");
     }
+
+    /**
+     * \brief Construct a new keyword record object.
+     * \tparam Value The type of the record's value. This is restricted to
+     * `int`, `float`, `std::complex<float>`, `std::string`, and `bool` by the
+     * FITS specification. Failure to adhere to these restrictions will result
+     * in a compile-time error.
+     * \param[in] key The key for the new record. The key must be between one
+     * and eight characters. The characters may only be digits, upper case
+     * characters, dashes, and underscores.
+     * \param[in] comment The comment for the new record. Comments are optional.
+     * An entire header keyword record cannot exceed 80 bytes, including space
+     * padding inserted by the library. Comments which would cause the record to
+     * exceed 80 bytes are truncated.
+     * \param[in] val The initial value of the record.
+     * \throws fits::invalid_key If \a key is not valid.
+     */
+    template <typename Value>
     keyword_record(const std::string& key,
                    const std::string& comment,
-                   const int val):
-      m_key {key},
-      m_comment {comment},
-      m_value {val}
+                   const std::remove_cv_t<Value> val):
+      m_key {key}, m_comment {comment}, m_value {val}
     {
+      static_assert(fits::detail::is_value<std::remove_cv_t<Value>>::value);
     }
 
     /**
-     * \brief Construct a header datum.
-     * \tparam T The type of the value. This is restricted to `int`, `float`,
-     * `std::complex<float>`, `std::string`, and `bool` by the FITS
-     * specification. Failure to adhere to these restrictions will result in a
-     * compile-time error.
-     * \param[in] key The key for the datum. Keys must be between 1 and 8
-     * characters, and are restricted to digits, upper case characters, dash,
-     * and underscore.
-     * \param[in] value The value for the datum.
-     * \param[in] comment A comment to record with the datum. Comments are
-     * optional. An entire header datum cannot exceed 80 bytes, including space
-     * padding inserted by the library. Comments which would cause the datum to
-     * exceed 80 bytes are truncated.
-     * \throws fits::invalid_key This is thrown if \a key violates any of the
-     * restrictions noted above.
+     * \brief Swap this keyword record with another.
+     * \param[in] other The other record to swap with this.
      */
-    // template <typename T>
-    // keyword_record(const std::string& key,
-    //               const T& val,
-    //               const std::string& comment = ""):
-    //  m_key {key},
-    //  m_comment {comment},
-    //  m_value {val}
-    //{
-    //  // clang-format off
-    //  static_assert(std::is_same_v<T, int>
-    //      || std::is_same_v<T, float>
-    //      || std::is_same_v<T, std::complex<float>>
-    //      || std::is_same_v<T, std::string>
-    //      || std::is_same_v<T, bool>);
-    //  // clang-format on
-    //  // FITS_EXPECTS(
-    //  //  !key.empty(), fits::invalid_key, "Header key may not be empty.");
-    //  // FITS_EXPECTS(key.size() <= 8,
-    //  //             fits::invalid_key,
-    //  //             "Header key may not exceed 8 characters.");
-    //  // FITS_EXPECTS(details::is_valid_header_key(key),
-    //  //             fits::invalid_key,
-    //  //             "Header key may not contain invalid ASCII characters.");
-    //}
-
-    // void swap(keyword_record& other) noexcept;
+    void swap(keyword_record& other) noexcept;
 
     /**
-     * \brief Get the datum's key.
+     * \brief Get the record's key.
      * \return The key.
      * \throws Unknown Anything thrown by copying a `std::string` is allowed to
      * propagate.
      */
     std::string key() const;
 
-    // bool has_a_value() const noexcept;
-
-    // template <typename T>
-    // void set_value(const T& new_value)
-    //{
-    //}
-
-    //    template <typename T>
-    //    fits::keyword_record& operator=(const T& new_value)
-    //    {
-    //      // GCC 8.3 appears to have a bug, in which -Weffc++ warns that this
-    //      // operator should return a reference to *this, even though it is.
-    //#if __GNUC__ == 8
-    //#  pragma GCC diagnostic push
-    //#  pragma GCC diagnostic ignored "-Weffc++"
-    //      return *this;
-    //#  pragma GCC diagnostic pop
-    //#endif
-    //    }
+    /**
+     * \brief Query if the record has a value.
+     * \retval true The record has a value.
+     * \retval false The record does not have a value.
+     */
+    bool has_a_value() const noexcept;
 
     /**
-     * \brief Get the datum's value.
-     * \tparam T The data type of the returned value. See
+     * \brief Set the record's value.
+     * \tparam Value The type of the new value. See keyword_record() for the
+     * restrictions on the Value type.
+     * \param[in] new_value The new value for the record.
+     */
+    template <typename Value>
+    void set_value(const Value& new_value)
+    {
+      static_assert(fits::detail::is_value<Value>::value);
+    }
+
+    /**
+     * \brief Set the record's value.
+     * \tparam Value The type of the new value. See keyword_record() for the
+     * restrictions on the Value type.
+     * \param[in] new_value The new value for the record.
+     */
+    template <typename Value>
+    fits::keyword_record& operator=(const Value& new_value)
+    {
+      this->set_value(new_value);
+      // GCC 8.3 appears to have a bug, in which -Weffc++ warns that this
+      // operator should return a reference to *this, even though it is.
+#if __GNUC__ == 8
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Weffc++"
+#endif
+      return *this;
+#if __GNUC__ == 8
+#  pragma GCC diagnostic pop
+#endif
+    }
+
+    /**
+     * \brief Get the record's value.
+     * \tparam Value The data type of the returned value. See
      * fits::keyword_record::keyword_record() for type restrictions.
-     * \return The datum's value.
+     * \return The record's value.
+     * \throws std::bad_variant_access This is thrown if \a Value does not match
+     * the type of the current value.
      * \throws Unknown Anything thrown by invalid access of a `std::variant` is
      * allowed to propagate.
      */
-    // template <typename T>
-    // T value() const
-    //{
-    //  // clang-format off
-    //  static_assert(std::is_same_v<T, int>
-    //      || std::is_same_v<T, float>
-    //      || std::is_same_v<T, std::complex<float>>
-    //      || std::is_same_v<T, std::string>
-    //      || std::is_same_v<T, bool>);
-    //  // clang-format on
-    //  return std::get<T>(m_value);
-    //}
-
-    // bool has_a_comment() const noexcept;
-    // void set_comment(const std::string& new_comment);
+    template <typename Value>
+    Value value() const
+    {
+      static_assert(fits::detail::is_value<Value>::value);
+      return std::get<Value>(m_value);
+    }
 
     /**
-     * \brief Get the datum's comment.
+     * \brief Query if the record has a comment.
+     * \retval true The record has a comment.
+     * \retval false The record does not have a comment.
+     */
+    bool has_a_comment() const noexcept;
+
+    /**
+     * \brief Set the record's comment.
+     * \param[in] new_comment The new comment.
+     */
+    void set_comment(const std::string& new_comment);
+
+    /**
+     * \brief Get the record's comment.
      * \return The comment.
      * \throws Unknown Anything thrown by copying a `std::string` is allowed to
      * propagate.
@@ -145,15 +176,45 @@ namespace fits
 
   private:
     std::string m_key;
-    std::string m_comment {""};
     std::variant<int, float, std::complex<float>, std::string, bool> m_value {
       0};
+    std::string m_comment {""};
   };
 
+  /**
+   * \brief Compare two keyword records for equality.
+   * \param[in] a,b The two records to compare.
+   * \retval true Both records of identical keys, comments, and values. If one
+   * record does not have a comment or value, the other record also does not
+   * have a comment or value.
+   * \retval false The two records do not have identical keys, comments, or
+   * values.
+   */
   bool operator==(const fits::keyword_record& a,
                   const fits::keyword_record& b) noexcept;
+
+  /**
+   * \brief Compare two keyword records for inequality.
+   * \param[in] a,b The two records to compare.
+   * \retval true The two records do not have identical keys, comments, or
+   * values.
+   * \retval false Both records of identical keys, comments, and values. If one
+   * record does not have a comment or value, the other record also does not
+   * have a comment or value.
+   */
   bool operator!=(const fits::keyword_record& a,
                   const fits::keyword_record& b) noexcept;
+
+  /**
+   * \brief Query if one keyword record is less than another.
+   * \param[in] a,b The two records to compare.
+   * \retval true \a
+   * \details Record data is compared in the following order:
+   * - key
+   * - value
+   * - comment
+   *
+   */
   bool operator<(const fits::keyword_record& a,
                  const fits::keyword_record& b) noexcept;
   bool operator<=(const fits::keyword_record& a,
